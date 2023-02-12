6de957f276a14b60d1c1260a666950d274255b30
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-12 02:40:22 -0300

    Criando o Helper para pegar o tamanho da tela do dispositivo

---
+++ app/lib/app/core/ui/helpers/size_extensions.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+extension SizeExtension on BuildContext {
+  double get screenWidth => MediaQuery.of(this).size.width;
+  double get screenHeight => MediaQuery.of(this).size.height;
+
+  double percentWidth(double percent) => screenWidth * percent;
+  double percentHeight(double percent) => screenHeight * percent;
+}

063debf8660f19b233845aa9a807aa46577d66c0
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-12 02:39:16 -0300

    Adicionando o Env para acessar o backend

---
+++ app/lib/app/core/config/env/env.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class Env {
+  static Env? _instance;
+  Env._();
+
+  static Env get i {
+    _instance ??= Env._();
+    return _instance!;
+  }
+
+  Future<void> load() => dotenv.load();
+
+  String? operator [](String key) => dotenv.env[key];
+}

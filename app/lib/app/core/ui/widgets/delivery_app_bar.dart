3ac6f2979d755e8f4ce88f6f92c5e325ede9dd6f
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-13 02:26:38 -0300

    Criando o widget de AppBar

---
+++ app/lib/app/core/ui/widgets/delivery_app_bar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DeliveryAppBar extends AppBar {
+  DeliveryAppBar({
+    super.key,
+    double elevation = 1,
+  }) : super(
+          elevation: elevation,
+          title: Image.asset(
+            'assets/images/logo.png',
+            width: 80,
+          ),
+        );
+}

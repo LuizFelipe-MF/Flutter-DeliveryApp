d46545ff433d89d034dcab969e04fb0ee8abd153
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-13 02:24:21 -0300

    Criando o modelo do produto

---
+++ app/lib/app/models/product_model.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+class ProductModel {
+  final int id;
+  final String name;
+  final String description;
+  final double price;
+  final String image;
+
+  ProductModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.image,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'description': description,
+      'price': price,
+      'image': image,
+    };
+  }
+
+  factory ProductModel.fromMap(Map<String, dynamic> map) {
+    return ProductModel(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      description: map['description'] as String,
+      price: map['price'] as double,
+      image: map['image'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ProductModel.fromJson(String source) =>
+      ProductModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}

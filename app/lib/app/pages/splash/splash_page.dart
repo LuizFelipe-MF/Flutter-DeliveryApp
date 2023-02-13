833f50ec2b69e417a41b4c98bc2a7425f504f44c
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-13 02:21:23 -0300

    Criando a SplashScreen

--- app/lib/app/pages/splash/splash_page.dart
+++ app/lib/app/pages/splash/splash_page.dart
@@ -1,3 +1,5 @@
+import 'package:app/app/core/ui/helpers/size_extensions.dart';
+import 'package:app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -8,8 +10,44 @@ class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Splash')),
-      body: Container(),
+      body: ColoredBox(
+        color: const Color(0XFF140E0E),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.screenWidth,
+                child: Image.asset(
+                  'assets/images/lanche.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: context.percentHeight(.30),
+                  ),
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  DeliveryButton(
+                    width: context.percentWidth(.60),
+                    height: 35,
+                    label: 'ACESSAR',
+                    onPressed: () {
+                      Navigator.of(context).popAndPushNamed('/home');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

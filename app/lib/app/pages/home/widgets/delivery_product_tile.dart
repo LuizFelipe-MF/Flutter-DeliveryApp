a358897b96c4e8fe9dc55ca51a2b85947de8d31d
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-17 10:42:10 -0300

    Tornando o cartão de produto clicável

--- app/lib/app/pages/home/widgets/delivery_product_tile.dart
+++ app/lib/app/pages/home/widgets/delivery_product_tile.dart
@@ -11,50 +11,58 @@ class DeliveryProductTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: Text(
-                    product.name,
-                    style:
-                        context.textStyle.textExtraBold.copyWith(fontSize: 16),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: Text(
-                    product.description,
-                    style: context.textStyle.textRegular.copyWith(fontSize: 13),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: Text(
-                    product.price.currencyPTBR,
-                    style: context.textStyle.textMedium.copyWith(
-                      fontSize: 12,
-                      color: context.colors.secondary,
+    return InkWell(
+      onTap: () async {
+        await Navigator.of(context).pushNamed('/productDetail', arguments: {
+          'product': product,
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Text(
+                      product.name,
+                      style: context.textStyle.textExtraBold
+                          .copyWith(fontSize: 16),
                     ),
                   ),
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Text(
+                      product.description,
+                      style:
+                          context.textStyle.textRegular.copyWith(fontSize: 13),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Text(
+                      product.price.currencyPTBR,
+                      style: context.textStyle.textMedium.copyWith(
+                        fontSize: 12,
+                        color: context.colors.secondary,
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          FadeInImage.assetNetwork(
-            placeholder: 'assets/images/loading.gif',
-            image: product.image,
-            width: 100,
-            height: 100,
-            fit: BoxFit.cover,
-          )
-        ],
+            FadeInImage.assetNetwork(
+              placeholder: 'assets/images/loading.gif',
+              image: product.image,
+              width: 100,
+              height: 100,
+              fit: BoxFit.cover,
+            )
+          ],
+        ),
       ),
     );
   }

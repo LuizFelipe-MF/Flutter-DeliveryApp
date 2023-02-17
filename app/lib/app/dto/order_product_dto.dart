4166bad365026f0519aaef95f4f38418d1881b74
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-17 10:41:41 -0300

    Criando o DTO  de pedido

---
+++ app/lib/app/dto/order_product_dto.dart
@@ -0,0 +1,13 @@
+import 'package:app/app/models/product_model.dart';
+
+class OrderProductDto {
+  final ProductModel product;
+  final int amount;
+
+  OrderProductDto({
+    required this.product,
+    required this.amount,
+  });
+
+  double get totalPrice => amount * product.price;
+}

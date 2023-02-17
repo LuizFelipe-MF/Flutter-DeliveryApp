c9154a8102be7a950d99b8f59aa3e5b64e2e9342
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-17 10:42:28 -0300

    Criando o Bloc da página de productDetail

---
+++ app/lib/app/pages/product_detail/product_detail_controller.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ProductDetailController extends Cubit<int> {
+  ProductDetailController() : super(1);
+
+  void increment() => emit(state + 1);
+  void decrement() => {
+        if (state > 1) {emit(state - 1)}
+      };
+}

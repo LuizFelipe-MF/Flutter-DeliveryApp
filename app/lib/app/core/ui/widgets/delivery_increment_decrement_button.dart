c0ed2823fb59dfcb52ec0694df005713728e989c
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-17 10:41:23 -0300

    Criando widget do botão de incrementar e decrementar

---
+++ app/lib/app/core/ui/widgets/delivery_increment_decrement_button.dart
@@ -0,0 +1,63 @@
+import 'package:app/app/core/ui/styles/colors_app.dart';
+import 'package:app/app/core/ui/styles/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class DeliveryIncrementDecrementButton extends StatelessWidget {
+  final int amount;
+  final VoidCallback incrementTap;
+  final VoidCallback decrementTap;
+
+  const DeliveryIncrementDecrementButton({
+    super.key,
+    required this.amount,
+    required this.incrementTap,
+    required this.decrementTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(7)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          InkWell(
+            onTap: decrementTap,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                '-',
+                style: context.textStyle.textMedium.copyWith(
+                  fontSize: 22,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+          Text(
+            amount.toString(),
+            style: context.textStyle.textRegular.copyWith(
+              fontSize: 17,
+              color: context.colors.secondary,
+            ),
+          ),
+          InkWell(
+            onTap: incrementTap,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                '+',
+                style: context.textStyle.textMedium.copyWith(
+                  fontSize: 22,
+                  color: context.colors.secondary,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

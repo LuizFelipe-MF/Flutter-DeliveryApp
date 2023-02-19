826a8f127fb1ab58e79337baa12b75469a279342
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-19 03:02:22 -0300

    Criando widget de field da tela de pedidos

---
+++ app/lib/app/pages/order/widget/order_field.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:app/app/core/ui/styles/text_styles.dart';
+import 'package:flutter/material.dart';
+
+class OrderField extends StatelessWidget {
+  final String title;
+  final String hintText;
+  final TextEditingController controller;
+  final FormFieldValidator validator;
+
+  const OrderField({
+    super.key,
+    required this.title,
+    required this.hintText,
+    required this.controller,
+    required this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const defaultBorder = UnderlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.grey,
+      ),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 35,
+            child: Text(
+              title,
+              style: context.textStyle.textRegular.copyWith(
+                fontSize: 16,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: hintText,
+              border: defaultBorder,
+              enabledBorder: defaultBorder,
+              errorBorder: defaultBorder,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}

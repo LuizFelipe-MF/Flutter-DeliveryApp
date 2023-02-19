4dfdeee74af32850f6b911677cc68440932f2982
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-19 02:57:16 -0300

    Adicionando as rotas das telas de login, registro e pedido

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -2,7 +2,12 @@ import 'package:app/app/core/provider/aplication_binding.dart';
 import 'package:app/app/core/ui/styles/app_styles.dart';
 import 'package:app/app/core/ui/styles/colors_app.dart';
 import 'package:app/app/core/ui/theme/theme_config.dart';
+import 'package:app/app/pages/auth/login/login_page.dart';
+import 'package:app/app/pages/auth/login/login_router.dart';
+import 'package:app/app/pages/auth/register/register_page.dart';
+import 'package:app/app/pages/auth/register/register_router.dart';
 import 'package:app/app/pages/home/home_router.dart';
+import 'package:app/app/pages/order/order_page.dart';
 import 'package:app/app/pages/product_detail/product_detail_page.dart';
 import 'package:app/app/pages/product_detail/product_detail_router.dart';
 import 'package:app/app/pages/splash/splash_page.dart';
@@ -22,6 +27,9 @@ class App extends StatelessWidget {
           '/': (context) => const SplashPage(),
           '/home': (context) => HomeRouter.page,
           '/productDetail': (context) => ProductDetailRouter.page,
+          '/auth/login': (context) => LoginRouter.page,
+          '/auth/register': (context) => RegisterRouter.page,
+          '/order': (context) => const OrderPage(),
         },
         theme: ThemeConfig.theme,
       ),

b48cf47d4243c710444c32d0aeb5b2319cdbe34a
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-20 00:50:24 -0300

    Criando um AuthInterceptor e adicionando ao CustomDio

--- app/lib/app/core/rest_client/custom_dio.dart
+++ app/lib/app/core/rest_client/custom_dio.dart
@@ -1,8 +1,11 @@
 import 'package:app/app/core/config/env/env.dart';
+import 'package:app/app/core/rest_client/interceptors/auth_interceptor.dart';
 import 'package:dio/dio.dart';
 import 'package:dio/native_imp.dart';
 
 class CustomDio extends DioForNative {
+  late final AuthInterceptor _authInterceptor;
+
   CustomDio()
       : super(BaseOptions(
           baseUrl: Env.i['backend_base_url'] ?? '',
@@ -14,14 +17,16 @@ class CustomDio extends DioForNative {
       responseBody: true,
       requestHeader: true,
     ));
+    _authInterceptor = AuthInterceptor();
   }
 
   CustomDio auth() {
+    interceptors.add(_authInterceptor);
     return this;
   }
 
-  
   CustomDio unauth() {
+    interceptors.remove(_authInterceptor);
     return this;
   }
 }
---
+++ app/lib/app/core/rest_client/interceptors/auth_interceptor.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthInterceptor extends Interceptor {
+  @override
+  Future<void> onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    final sp = await SharedPreferences.getInstance();
+    final accessToken = sp.getString('accessToken');
+
+    options.headers['Authorization'] = 'Bearer $accessToken';
+
+    handler.next(options);
+  }
+
+  @override
+  Future<void> onError(DioError err, ErrorInterceptorHandler handler) async {
+    if (err.response?.statusCode == 401) {
+      final sp = await SharedPreferences.getInstance();
+      sp.clear();
+      handler.next(err);
+    } else {
+      handler.next(err);
+    }
+  }
+}

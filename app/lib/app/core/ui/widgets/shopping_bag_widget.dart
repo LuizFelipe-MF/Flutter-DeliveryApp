0552480f340483f4939ea6b409d1b6d3fb0b3453
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-22 00:32:37 -0300

    Atualizando sacola

--- app/lib/app/core/ui/widgets/shopping_bag_widget.dart
+++ app/lib/app/core/ui/widgets/shopping_bag_widget.dart
@@ -2,7 +2,9 @@ import 'package:app/app/core/extensions/formatter_extension.dart';
 import 'package:app/app/core/ui/helpers/size_extensions.dart';
 import 'package:app/app/core/ui/styles/text_styles.dart';
 import 'package:app/app/dto/order_product_dto.dart';
+import 'package:app/app/pages/home/home_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ShoppingBagWidget extends StatelessWidget {
@@ -14,6 +16,7 @@ class ShoppingBagWidget extends StatelessWidget {
   });
 
   Future<void> _goOrder(BuildContext context) async {
+    final controller = context.read<HomeController>();
     final navigator = Navigator.of(context);
     final sp = await SharedPreferences.getInstance();
 
@@ -25,7 +28,8 @@ class ShoppingBagWidget extends StatelessWidget {
       }
     }
 
-    await navigator.pushNamed('/order', arguments: bag);
+    final updateBag = await navigator.pushNamed('/order', arguments: bag);
+    controller.updateBag(updateBag as List<OrderProductDto>);
   }
 
   @override

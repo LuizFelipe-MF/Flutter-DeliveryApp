d9e02324ab0fe9c1ae2ce42c618d7019d9c13db2
LuizFelipe-MF <maggioni.luizfelipe@gmail.com>
2023-02-22 00:33:05 -0300

    Criando novo método para realizar pedido

--- app/lib/app/repositories/order/order_repository.dart
+++ app/lib/app/repositories/order/order_repository.dart
@@ -1,5 +1,7 @@
+import 'package:app/app/dto/order_dto.dart';
 import 'package:app/app/models/payment_types_model.dart';
 
 abstract class OrderRepository {
   Future<List<PaymentTypesModel>> getAllPaymentTypes();
+  Future<void> saveOrder(OrderDto order);
 }
--- app/lib/app/repositories/order/order_repository_impl.dart
+++ app/lib/app/repositories/order/order_repository_impl.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:app/app/core/exeptions/repository_exeption.dart';
 import 'package:app/app/core/rest_client/custom_dio.dart';
+import 'package:app/app/dto/order_dto.dart';
 import 'package:app/app/models/payment_types_model.dart';
 import 'package:dio/dio.dart';
 
@@ -21,8 +22,29 @@ class OrderRepositoryImpl implements OrderRepository {
       return result.data
           .map<PaymentTypesModel>((e) => PaymentTypesModel.fromMap(e))
           .toList();
-    } on DioError {
+    } on DioError catch (e) {
       throw RepositoryExeption(message: 'Erro ao buscar formas de pagamento.');
+    }
+  }
+
+  @override
+  Future<void> saveOrder(OrderDto order) async {
+    try {
+      await dio.auth().post('/orders', data: {
+        'products': order.products
+            .map((e) => {
+                  'id': e.product.id,
+                  'amount': e.amount,
+                  'totalPrice': e.totalPrice,
+                })
+            .toList(),
+        'user_id': '#userAuthRef',
+        'address': order.address,
+        'CPF': order.document,
+        'payment_method_id': order.paymentMethodId,
+      });
+    } on DioError catch (e) {
+      throw RepositoryExeption(message: 'Erro ao realizar pedido.'); 
     }
   }
 }
